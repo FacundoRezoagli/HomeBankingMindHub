fe2b9ef517f7cdad1bbe54f4b611b941b58d1265
Facundo <Facundo@DESKTOP-TQC2PQ2>
2024-02-03 18:47:24 -0300

    agregado inicializador de cuentas agregada migracion nueva

--- Models/DBInitializer.cs
+++ Models/DBInitializer.cs
@@ -16,10 +16,26 @@ namespace HomeBankingMindHub.Models
 
                 context.Clients.AddRange(clients);
 
-                //guardamos
                 context.SaveChanges();
-            }
+            }//CLIENTS
 
+            if (!context.Account.Any())
+            {
+                var accountVictor = context.Clients.FirstOrDefault(c => c.Email == "vcoronado@gmail.com");
+                if (accountVictor != null)
+                {
+                    var accounts = new Account[]
+                    {
+                        new Account {ClientId = accountVictor.Id, CreationDate = DateTime.Now, Number = string.Empty, Balance = 0 }
+                    };
+                    foreach (Account account in accounts)
+                    {
+                        context.Account.Add(account);
+                    }
+                    context.SaveChanges();
+
+                }
+            }//ACCOUNTS
         }
     }
 }
