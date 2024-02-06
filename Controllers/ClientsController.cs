c8ba6a001248fe729ad5ef4947010f3048202dc8
Facundo <Facundo@DESKTOP-TQC2PQ2>
2024-02-06 10:25:55 -0300

    tarea 4 terminada

--- Controllers/ClientsController.cs
+++ Controllers/ClientsController.cs
@@ -35,6 +35,14 @@ namespace HomeBankingMindHub.Controllers
                             Balance = ac.Balance,
                             CreationDate = ac.CreationDate,
                             Number = ac.Number
+                        }).ToList(),
+                        Loans = client.ClientLoans.Select(cl => new ClientLoanDTO
+                        {
+                            Id = cl.Id,
+                            LoanId = cl.LoanId,
+                            Name = cl.Loan.Name,
+                            Amount = cl.Amount,
+                            Payments = int.Parse(cl.Payments)
                         }).ToList()
                     };
                     clientsDTO.Add(newClientDTO);
@@ -69,6 +77,14 @@ namespace HomeBankingMindHub.Controllers
                         Balance = ac.Balance,
                         CreationDate = ac.CreationDate,
                         Number = ac.Number
+                    }).ToList(),
+                    Loans = client.ClientLoans.Select(cl => new ClientLoanDTO
+                    {
+                        Id = cl.Id,
+                        LoanId = cl.LoanId,
+                        Name = cl.Loan.Name,
+                        Amount = cl.Amount,
+                        Payments = int.Parse(cl.Payments)
                     }).ToList()
                 };
                 return Ok(clientDTO);
---
+++ Migrations/20240206123114_addLoanEntiity2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeBankingMindHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class addLoanEntiity2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Payment",
+                table: "ClientLoans",
+                newName: "Payments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Payments",
+                table: "ClientLoans",
+                newName: "Payment");
+        }
+    }
+}
